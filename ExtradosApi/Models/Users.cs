86aa699ff18e482b92a9e0248fbcc39b3373f6e7
LilAguh <aguezeochoa@gmail.com>
2024-12-13 23:53:37 -0300

    Modelo de usuarios completo

--- ExtradosApi/Models/Users.cs
+++ ExtradosApi/Models/Users.cs
@@ -2,5 +2,10 @@
 {
     public class Users
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public int Age { get; set; }
+        public int Active { get; set; }
     }
 }
