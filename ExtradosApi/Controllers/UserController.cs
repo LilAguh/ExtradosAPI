5d42ae306ceefa58f48ed93732fdbf6964cca0bd
LilAguh <aguezeochoa@gmail.com>
2024-12-15 17:37:30 -0300

    Controlador de usuarios

---
+++ ExtradosApi/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using ExtradosApi.DAOs;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExtradosApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly DAOUsers _daousers;
+        public UserController()
+        {
+            _daousers = new DAOUsers();
+        }
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            var users = _daousers.GetAllUsersRegistered();
+            return Ok(users);
+        }
+    }
+}
