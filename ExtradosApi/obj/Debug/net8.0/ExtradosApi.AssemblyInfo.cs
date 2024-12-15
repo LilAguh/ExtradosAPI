4b3062ddbbc71fee1bec353d9484cc1fb6ec5bd4
LilAguh <aguhochoa@gmail.com>
2024-12-15 02:39:59 -0300

    actualizacion de nuget

--- ExtradosApi/obj/Debug/net8.0/ExtradosApi.AssemblyInfo.cs
+++ ExtradosApi/obj/Debug/net8.0/ExtradosApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExtradosApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86aa699ff18e482b92a9e0248fbcc39b3373f6e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2799cded6086d08b42612f021431ee7990fdbd77")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExtradosApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExtradosApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
