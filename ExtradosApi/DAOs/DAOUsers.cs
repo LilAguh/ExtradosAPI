4ce02d503000c674a909a697c567c297d2cadfe2
LilAguh <aguezeochoa@gmail.com>
2024-12-19 06:23:58 -0300

    Se completa todas las acciones del dao de usuarios

--- ExtradosApi/DAOs/DAOUsers.cs
+++ ExtradosApi/DAOs/DAOUsers.cs
@@ -6,8 +6,31 @@ namespace ExtradosApi.DAOs
 {
     public class DAOUsers
     {
-        
-        string connectionString = "Server=127.0.0.1;Database=ApiEjercicio;UId=aguh;Password=asd123;";
+        private static DAOUsers _instance; // Única instancia de la clase
+        private static readonly object _lock = new object(); // Objeto para sincronización
+
+        private string connectionString = "Server=127.0.0.1;Database=ApiEjercicio;UId=aguh;Password=asd123;";
+
+        // Constructor privado para evitar instanciación directa
+        private DAOUsers() { }
+
+        // Propiedad para obtener la instancia única
+        public static DAOUsers Instance
+        {
+            get
+            {
+                lock (_lock) // Bloqueo para garantizar que solo un hilo cree la instancia
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new DAOUsers();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        // Métodos CRUD
         public List<Users> GetAllUsersRegistered()
         {
             string query = "SELECT * FROM Users;";
@@ -17,6 +40,7 @@ namespace ExtradosApi.DAOs
                 return users;
             }
         }
+
         public List<Users> GetAllUsersActive()
         {
             string query = "SELECT * FROM Users WHERE active = 1;";
@@ -26,6 +50,7 @@ namespace ExtradosApi.DAOs
                 return users;
             }
         }
+
         public Users GetUser(int id)
         {
             string query = "SELECT * FROM Users where id = @id and active = 1;";
@@ -35,14 +60,15 @@ namespace ExtradosApi.DAOs
                 return user;
             }
         }
-        public Users CreateUser(string name, int age)
+
+        public Users CreateUser(string name, string mail, int age)
         {
-            string query = "INSERT INTO Users (name, age) VALUES (@name, @age);";
+            string query = "INSERT INTO Users (name, mail, age) VALUES (@name, @mail, @age);";
             string queryUserCreated = "SELECT * FROM Users WHERE id = LAST_INSERT_ID();";
             using (var connection = new MySqlConnection(connectionString))
             {
                 connection.Open();
-                connection.Execute(query, new { name = name, age = age });
+                connection.Execute(query, new { name = name, mail = mail, age = age });
                 var user = connection.QueryFirstOrDefault<Users>(queryUserCreated);
                 return user;
             }
@@ -55,19 +81,17 @@ namespace ExtradosApi.DAOs
             using (var connection = new MySqlConnection(connectionString))
             {
                 connection.Open();
-
-                //Se buscan los datos originales y en caso de no quere hacer cambio a alguno se deja el original
                 var user = connection.QueryFirstOrDefault<Users>(queryUpdateUser, new { id = id });
                 string newName = name ?? user.Name;
                 int newAge = age ?? user.Age;
                 string newMail = mail ?? user.Mail;
-
 
                 connection.Execute(query, new { name = newName, age = newAge, mail = newMail, id = id });
                 var userUpdate = connection.QueryFirstOrDefault<Users>(queryUpdateUser, new { id = id });
                 return userUpdate;
             }
         }
+
         public bool DesactivateUser(int id)
         {
             string query = "UPDATE User SET active = 0 WHERE id = @id;";
@@ -78,7 +102,6 @@ namespace ExtradosApi.DAOs
                 return userDeleted > 0;
             }
         }
-
     }
 }
 
