0622af8881a0825c3f3bc18066bba6b46a0e90d7
LilAguh <aguezeochoa@gmail.com>
2024-12-24 14:51:07 -0300

    Se integran las bibleotecas de clases a program

--- ExtradosApi/Program.cs
+++ ExtradosApi/Program.cs
@@ -1,3 +1,8 @@
+using DataAccess.Implementations;
+using DataAccess.Interfaces;
+using ExtradosApi.Services;
+using ExtradosApi.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IUserDAO, UserDAO>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
