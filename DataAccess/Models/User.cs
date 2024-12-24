7bc6538fa9db1856b419ffaa516285f6c784793f
LilAguh <aguezeochoa@gmail.com>
2024-12-24 14:51:33 -0300

    Los modelos ahora son parte de la bibleoteca de clases

---
+++ DataAccess/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Interfaces;
+using DataAccess.Implementations;
+
+namespace DataAccess.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Mail { get; set; } = string.Empty;
+        public bool Active { get; set; }
+    }
+}
