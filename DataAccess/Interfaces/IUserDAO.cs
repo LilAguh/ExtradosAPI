0fe3e433310c2588920dab7820f4139f3d9449ca
LilAguh <aguezeochoa@gmail.com>
2024-12-26 12:04:16 -0300

    en la interface del dao el viejo getUser se agrega pass y mail, el viejo se mantiene pero con el nombre getuserbyid

--- DataAccess/Interfaces/IUserDAO.cs
+++ DataAccess/Interfaces/IUserDAO.cs
@@ -6,7 +6,8 @@ namespace DataAccess.Interfaces
     {
         List<User> GetAllUsersRegistered();
         List<User> GetAllActiveUsers();
-        User GetUser(int id);
+        User GetUser(string mail, string password);
+        User GetUserById(int id);
         User CreateUser(string name,string password, string mail, int age); // Actualizar firma aquí
         User UpdateUser(int id, string? name = null, int? age = null, string? mail = null);
         bool DesactivateUser(int id);
