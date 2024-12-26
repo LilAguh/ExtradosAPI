e62d24780197e972dfd51ca94206ef69b9b7eb4a
LilAguh <aguezeochoa@gmail.com>
2024-12-26 11:25:39 -0300

    se agrega el string de password como interface de createUser

--- ExtradosApi/Services/Interfaces/IUserService.cs
+++ ExtradosApi/Services/Interfaces/IUserService.cs
@@ -7,7 +7,7 @@ namespace ExtradosApi.Services.Interfaces
         List<User> GetAllUsersRegistered();
         List<User> GetAllActiveUsers();
         User GetUser(int id);
-        User CreateUser(string name, string mail, int age);
+        User CreateUser(string name, string password, string mail, int age);
         User UpdateUser(int id, string? name = null, int? age = null, string? mail = null);
         bool DeactivateUser(int id);
     }
