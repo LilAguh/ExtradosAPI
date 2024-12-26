f2ddbd475b8860689df553de804c9b54df7dd43b
LilAguh <aguezeochoa@gmail.com>
2024-12-26 10:26:47 -0300

    Se añade el parametro password a create user

--- DataAccess/Interfaces/IUserDAO.cs
+++ DataAccess/Interfaces/IUserDAO.cs
@@ -7,7 +7,7 @@ namespace DataAccess.Interfaces
         List<User> GetAllUsersRegistered();
         List<User> GetAllActiveUsers();
         User GetUser(int id);
-        User CreateUser(string name, string mail, int age); // Actualizar firma aquí
+        User CreateUser(string name,string password, string mail, int age); // Actualizar firma aquí
         User UpdateUser(int id, string? name = null, int? age = null, string? mail = null);
         bool DesactivateUser(int id);
     }
