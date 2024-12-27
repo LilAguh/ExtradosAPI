b15226c2d62b37472b23e3dc75e6a0bbd310583f
LilAguh <aguezeochoa@gmail.com>
2024-12-27 19:01:23 -0300

    Se agrega la validacion por jwt

--- ExtradosApi/Program.cs
+++ ExtradosApi/Program.cs
@@ -2,6 +2,9 @@ using DataAccess.Implementations;
 using DataAccess.Interfaces;
 using ExtradosApi.Services;
 using ExtradosApi.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +17,25 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IUserDAO, UserDAO>();
 builder.Services.AddScoped<IUserService, UserService>();
 
+
+//paso 1: configurar la validación por JWT
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "yourcode.com",   // “yourco.com” es el issuer/audience
+            ValidAudience = "yourcode.com", // “yourco.com” es el issuer/audience
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("secret_secret_secret"))
+            //codigo secreto que se debe guardar
+        };
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +44,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+ //paso 2: declarar que vamos a utilizar el middleware de autorización y autenticación de .net
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
