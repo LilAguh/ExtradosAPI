9cde9b7e489c2a226bd95a8db16e2ab39b470cd2
LilAguh <aguezeochoa@gmail.com>
2024-12-27 17:03:09 -0300

    Al Modelo se le agrega la posibilidad de resolver con null la falta de parametros, necesario para actualizar los usuarios

--- DataAccess/Models/User.cs
+++ DataAccess/Models/User.cs
@@ -1,15 +1,16 @@
 ﻿using DataAccess.Interfaces;
 using DataAccess.Implementations;
+using System.Xml.Linq;
 
 namespace DataAccess.Models
 {
     public class User
     {
         public int ID { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public int Age { get; set; }
-        public string Mail { get; set; } = string.Empty;
-        public bool Active { get; set; }
-        public string Password { get; set; }
+        public string? Name { get; set; }
+        public string? Mail { get; set; }
+        public int? Age { get; set; } // Edad opcional
+        public string? Password { get; set; }
+        public bool Active { get; set; } = true;
     }
 }
