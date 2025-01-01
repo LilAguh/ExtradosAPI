9f8e88207d7f03a0c932a08aa122c74bc4fd33c7
LilAguh <aguezeochoa@gmail.com>
2025-01-01 20:00:29 -0300

    Se completa el dao de los libros

--- DataAccess/Implementations/BookLoanDAO.cs
+++ DataAccess/Implementations/BookLoanDAO.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DataAccess.Interfaces;
+using DataAccess.Models;
+using MySqlConnector;
+using Dapper;
+using Microsoft.Extensions.Configuration;
 
 namespace DataAccess.Implementations
 {
-    internal class BookLoanDAO
+    public class BookLoanDAO : IBookLoanDAO
     {
+        private readonly string connectionString;
+
+        public BookLoanDAO (IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public BookLoan CreateBookLoan (string bookName, int userId, DateTime loanDate, DateTime dueDate)
+        {
+            string query = "INSERT INTO BookLoans (bookname, userid, loandate, duedate) VALUES (@BookName, @UserID, @LoanDate, @DueDate);";
+            string queryLastLoan = "SELECT * FROM BookLoans WHERE ID = LAST_INSERT_ID();";
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                connection.Execute(query, new { BookName = bookName, UserID = userId, LoanDate = loanDate, DueDate = dueDate });
+                return connection.QueryFirstOrDefault<BookLoan>(queryLastLoan);
+            }
+        }
+
+        public List<BookLoan> GetBookLoansByUserId( int userId)
+        {
+            string query = "SELECT * FROM BookLoans WHERE userid = @UserId";
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                return connection.Query<BookLoan>(query, new { UserID = userId }).AsList();
+            }
+        }
+
+        public bool ReturnBook( int loanId)
+        {
+            string query = "UPDATE BookLoans SET returned = 1 WHERE id = @LoanID;";
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                return connection.Execute(query, new { LoanID = loanId }) > 0;
+            }
+        }
     }
 }
