b075f086fa49413c75955068171b3143885bc5bf
LilAguh <aguezeochoa@gmail.com>
2025-01-01 21:09:54 -0300

    Se crea la interfaz para el servicio

---
+++ ExtradosApi/Services/Interfaces/IBookLoanService.cs
@@ -0,0 +1,11 @@
+﻿using DataAccess.Models;
+
+namespace ExtradosApi.Services.Interfaces
+{
+    public interface IBookLoanService
+    {
+        BookLoan CreateBookLoan(string bookName, int userId, DateTime loanDate, DateTime dueDate);
+        List<BookLoan> GetBookLoansByUserId(int userId);
+        bool ReturnBook(int loanId);
+    }
+}
