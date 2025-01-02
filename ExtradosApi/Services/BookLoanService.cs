d73cb6087b02cefa70ce7d3a2b7e0a4be482cc8c
LilAguh <aguezeochoa@gmail.com>
2025-01-01 21:09:30 -0300

    Se crea el servicio para los libros

---
+++ ExtradosApi/Services/BookLoanService.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.Interfaces;
+using DataAccess.Implementations;
+using DataAccess.Models;
+using ExtradosApi.Services.Interfaces;
+
+namespace ExtradosApi.Services
+{
+    public class BookLoanService
+    {
+        private readonly IBookLoanDAO _bookLoanDAO;
+
+        public BookLoanService(IBookLoanDAO bookLoanDAO)
+        {
+            _bookLoanDAO = bookLoanDAO;
+        }
+
+        public BookLoan CreateBookLoan(string bookName, int userId)
+        {
+            DateTime loanDate = DateTime.Now;
+            DateTime dueDate = loanDate.AddDays(5);
+
+            return _bookLoanDAO.CreateBookLoan(bookName, userId, loanDate, dueDate);
+        }
+
+        public List<BookLoan> GetBookLoansByUserId(int userId)
+        {
+            return _bookLoanDAO.GetBookLoansByUserId(userId);
+        }
+
+        public bool ReturnBook(int loanId)
+        {
+            return _bookLoanDAO.ReturnBook(loanId);
+        }
+    }
+}
